9ee664a6b644e070f95f956486c0ba4637f2f3e8
Tiago Lança <tiagofilipe.lanca@gmail.com>
2024-11-05 00:47:06 +0000

    Save/Load Game / Piece Class

--- obj/Debug/net8.0/Chess_Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Chess_Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess_Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea03237f1ee4bdcf3b60e4170b5e850481812e9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700f9be90d44915e2f11aae0ef538671d68ba541")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess_Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chess_Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
